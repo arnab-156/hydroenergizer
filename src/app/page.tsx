4f47392b59f88373d897f2376733d37e84295a3d
arnab-156 <vendmyt@gmail.com>
2024-11-25 19:24:31 -0600

    public/images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,56 +6,91 @@ import OutlinedButton from "./components/button/outlineButton";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-full px-2 relative space-y-4 max-w-8xl mx-auto px-4 sm:px-6 md:px-8">
-
-      <div className="group flex flex-col items-center justify-between border-2 border-red rounded-2xl mx-2 mt-4 h-[calc(95%)] fixed top-0 left-0 right-0 bottom-0">
+    <div className={`
+      font-mono flex flex-col w-full relative space-y-4 max-w-8xl 
+      px-2 mx-auto px-4 px-0 md:px-8`}>
+      {/* Outer Blk line wrapper */}
+      <div
+        className={
+          `group flex flex-col items-center
+            justify-between border-2 border-black
+            fixed top-0 left-0 right-0 bottom-0
+            sm:mx-0 sm:mt-0 h-[calc(100%)]  
+            md:mx-2 md:rounded-2xl md:mt-4 md:h-[calc(95%)]   
+          `
+        }>
         <Navigation />
-        {/* Blue line wrapper */}
-        <div className="border border-5 border-black rounded-2xl overflow-y-auto w-[calc(100%-2rem)] h-[calc(95%-3rem)] mt-16 ">
+        {/* Inner Blk line wrapper */}
+        <div className={
+          `border border-5 border-black overflow-y-auto 
+          mt-16 h-[calc(95%-3rem)] 
+          sm:w-full
+          md:w-[calc(100%-2rem)] md:rounded-2xl
+          `
+        }>
 
           {/* hero */}
           <div className="sticky top-0 rounded-2xl">
             <Image
-              className="object-none object-center rounded-2xl w-full h-200 z-10 md:object-cover"
-              src="/images/home/mountains.jpg"
+              className="w-full h-full"
+              src="/images/home/HydroVid.png"
               alt="Hydrogen world"
               priority
               unoptimized
             />
-            <div className="absolute top-1/4 left-1/2 transform -translate-x-[50%] flex flex-col justify-center fixed">
+
+
+
+            {/* <video
+              controls autoPlay
+              className="w-full h-full"
+              loop
+            >
+              <source src="/images/home/HydroVid.mp4" type="video/mp4" />
+            </video> */}
+
+
+
+            {/* <div className="absolute top-1/4 left-1/2 transform -translate-x-[50%] flex flex-col justify-center fixed">
               <Image
                 className="rounded-2xl"
-                src="/videos/Hydro.png"
-                alt="logo of commander's palace"
+                src="/images/home/hydro_name.svg"
+                alt="logo of hydro's palace"
                 priority
                 unoptimized
               />
 
               <div className="flex justify-center m-auto mt-6">
-                <OutlinedButton title="Make a Reservation" url="/" backgroundColor="background" />
+                <OutlinedButton title="Contact us" url="/" backgroundColor="background" />
               </div>
-            </div>
+            </div> */}
           </div>
 
-          <main className="flexflex-col items-center bg-background rounded-b-2xl h-full overflow-y-auto w-[calc(100%)] px-0 pt-8 sticky top-0 z-30">
-            <section className="h-48 overflow-y-auto">
+          <main className={`
+            flexflex-col items-center bg-background rounded-b-2xl h-full 
+            overflow-y-auto w-screen px-4 pt-8 
+            sticky top-0 z-30 content-center`}>
+            <section className={`h-48 overflow-y-auto flex flex-col`}>
               {/* Div is some text and a styled button, desktop has two images */}
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.
+              </p>
 
-              <div className={`border rounded-md border-leaf`}>
+              <div className={`rounded-lg border border-2 border-black w-64`}>
                 <Link className={
                   `uppercase flex justify-center items-center rounded-md 
-                text-background bg-leaf
+                text-background bg-black
                 scale-x-95 scale-y-90 
                 p-2 md:p-4 hover:scale-100 transition delay-100 duration-700 ease-in-out`}
                   href={"/"}>
-                  View Menu
+                  Learn About Hydro Energizer
                 </Link>
               </div>
             </section>
 
 
-            <section className="h-96 border border-1">
+            <section className={`h-48 overflow-y-auto`}>
               {/* Div is background image with flush card with double outline (headline, title, para, button) */}
               <div>
                 Paragraph 1:
@@ -80,7 +115,7 @@ export default function Home() {
             </section>
 
 
-            <section className="h-48 overflow-y-auto">
+            <section className={`h-48 overflow-y-auto`}>
               {/* Div is some text, a styled button, and two images */}
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
@@ -95,11 +130,6 @@ export default function Home() {
                 </Link>
               </div>
             </section>
-
-
-
-
-
 
             <Footer />
           </main>
