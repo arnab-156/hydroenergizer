b7f5f18ef5527909f4e7c6b15407c4e92fb01264
arnab-156 <vendmyt@gmail.com>
2024-11-26 09:25:01 -0600

    added some styling to slideing

--- src/app/components/navigation/desktop.tsx
+++ src/app/components/navigation/desktop.tsx
@@ -1,24 +1,25 @@
 import Link from 'next/link';
 
 const DesktopNav = () => {
+    const $className = "hidden md:flex justify-center space-around items-center px-2 m-2 h-10 hover:underline";
+
     return (
         <nav className="hidden md:flex flex-row-reverse md:flex-row flex-grow w-[calc(100%-2rem)] fixed z-10 justify-evenly dark:invert">
-            <Link className="hidden md:flex justify-center space-around items-center px-2 m-2 h-10 hover:underline" href="/">
+            <Link className={$className} href="/">
                 Home
             </Link>
-            <Link className="hidden md:flex justify-center items-center px-2 m-2 h-10 hover:underline" href="#about-me">
+            <Link className={$className} href="#about-me">
                 About
             </Link>
-            <Link className="hidden md:flex justify-center items-center px-2 m-2 h-10 hover:underline" href="#social">
+            <Link className={$className} href="#social">
                 Social
             </Link>
-            <Link className="hidden md:flex justify-center items-center px-2 m-2 h-10 hover:underline" href="/">
+            <Link className={$className} href="/">
                 Shop
             </Link>
-            <Link className="hidden md:flex justify-center items-center px-2 m-2 h-10 hover:underline" href="#contact">
+            <Link className={$className} href="#contact">
                 Contact
             </Link>
-
         </nav>
     )
 }
